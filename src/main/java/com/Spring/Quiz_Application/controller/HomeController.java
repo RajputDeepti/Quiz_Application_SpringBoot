2db06e65512c61bcc500808ba9fb042e3bd768e2
gem-deeptilodhi <126055117+gem-deeptilodhi@users.noreply.github.com>
2023-08-10 18:53:55 +0530

    Update HomeController.java

--- src/main/java/com/Spring/Quiz_Application/controller/HomeController.java
+++ src/main/java/com/Spring/Quiz_Application/controller/HomeController.java
@@ -1,11 +1,5 @@
-
 package com.Spring.Quiz_Application.controller;
-
-
-
-
 import com.Spring.Quiz_Application.Dto.SignUpPageDto;
-
 import com.Spring.Quiz_Application.entity.User;
 import com.Spring.Quiz_Application.repository.UserRepository;
 import com.Spring.Quiz_Application.utils.CustomMailSender;
@@ -15,16 +9,11 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.Spring.Quiz_Application.entity.Question;
-
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-
 import java.util.UUID;
 
 @RestController
@@ -34,54 +23,55 @@ public class HomeController {
     private CustomMailSender mailSender;
     @Autowired
     private JavaMailSender javaMailSender;
-    @GetMapping("/register")
-    public  ModelAndView showRegistrationForm(Model model) {
-        model.addAttribute("user", new SignUpPageDto()); // Create a SignUpPageDto class to hold the form data
-        return new ModelAndView("SignupForm");
-    }
-
-    @GetMapping({"/","/home"})
-
-    public ModelAndView homeHandler(){
-        return new ModelAndView("home");
-    }
-    @GetMapping("/host/dashboard")
-    public ModelAndView hostDashboard() {
-        return new ModelAndView("HostDashboard");
-    }
-    @GetMapping("/candidate/dashboard")
-    public ModelAndView candidateDashboard() {
-        return new ModelAndView("CandidateDashboard");
-    }
-    @GetMapping("/signin")
-    public ModelAndView showLoginForm(Model model) {
-        return new ModelAndView("LoginForm");
-    }
-    @GetMapping("/access-denied")
-    public ModelAndView authenticationFailed(){
-        return new ModelAndView("AccessDeniedPage");
-    }
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    
+    @GetMapping("/register")
+    public  ModelAndView showRegistrationForm(Model model) {
+        model.addAttribute("user", new SignUpPageDto()); 
+        return new ModelAndView("SignupForm");
+    }
+
+    @GetMapping({"/","/home"})
+    public ModelAndView homeHandler(){
+        return new ModelAndView("home");
+    }
+    
+    @GetMapping("/host/dashboard")
+    public ModelAndView hostDashboard() {
+        return new ModelAndView("HostDashboard");
+    }
+    
+    @GetMapping("/candidate/dashboard")
+    public ModelAndView candidateDashboard() {
+        return new ModelAndView("CandidateDashboard");
+    }
+    
+    @GetMapping("/signin")
+    public ModelAndView showLoginForm(Model model) {
+        return new ModelAndView("LoginForm");
+    }
+    
+    @GetMapping("/access-denied")
+    public ModelAndView authenticationFailed(){
+        return new ModelAndView("AccessDeniedPage");
+    }
+    
     @GetMapping("/show-Change-Password")
     public ModelAndView openSettings(){
         return new ModelAndView("ChangePasswordForm");
     }
+    
     @PostMapping("/settings/change-password")
     public ModelAndView processChangePassword( String email, @RequestParam("oldPassword") String oldPassword, @RequestParam("newPassword") String newPassword, Model model) {
         User existingUser = userRepository.findByEmail(email);
         if (existingUser != null) {
-            // Check if the old password matches the stored password
             if (passwordEncoder.matches(oldPassword, existingUser.getPassword())) {
-                // Encrypt the new password and set it in the user entity
                 String encryptedPassword = passwordEncoder.encode(newPassword);
                 existingUser.setPassword(encryptedPassword);
-
-                // Save the updated user entity with the new password
                 userRepository.save(existingUser);
-
                 model.addAttribute("successMessage", "Password changed successfully!");
             } else {
                 model.addAttribute("errorMessage", "Old password is incorrect!");
@@ -91,6 +81,7 @@ public class HomeController {
         }
         return new ModelAndView("ChangePasswordForm");
     }
+    
     @GetMapping("/forgot-password")
     public ModelAndView showForgotPasswordForm() {
         return new ModelAndView("ForgotPasswordForm");
@@ -147,12 +138,6 @@ public class HomeController {
             model.addAttribute("errorMessage", "Invalid or expired reset token.");
         }
         return new ModelAndView("LoginForm");
-    }
-
-    @GetMapping("/addQuestion")
-    public ModelAndView showAddQuestionForm(Model model){
-        model.addAttribute("question",new Question());
-        return new ModelAndView("AddQuestions");
     }
 }
 
